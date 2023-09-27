895b5f821c3af553af14beb37253d2d7e0f13150
david18012003 <bravocarmen264@gmail.com>
2023-09-27 16:42:16 -0500

    slider

--- cesar/js/slither.js
+++ cesar/js/slither.js
@@ -25,6 +25,10 @@
 // })
 let atras = document.getElementById("atras");
 let siguiente = document.getElementById("siguiente");
+let signal1 = document.getElementById("signal1");
+let signal2 = document.getElementById("signal2");
+let signal3 = document.getElementById("signal3");
+let signal4 = document.getElementById("signal4");
 
 atras.addEventListener("click", function(){
     if (boxSlither.classList.contains("box-slither-4")) {
@@ -48,7 +52,7 @@ atras.addEventListener("click", function(){
 siguiente.addEventListener("click", function(){
     if (boxSlither.classList.contains("box-slither-1")) {
         boxSlither.classList.remove("box-slither-1");
-        boxSlither.classList.add("box-slither-2"); 
+        boxSlither.classList.add("box-slither-2");
     }
     else if (boxSlither.classList.contains("box-slither-2")) {
         boxSlither.classList.remove("box-slither-2");
@@ -64,5 +68,137 @@ siguiente.addEventListener("click", function(){
     }
     
 })
+
+
+siguiente.addEventListener("click", function(){
+    if (signal1.classList.contains("sig1")) {
+        signal1.classList.remove("sig1");
+        signal1.classList.add("sig2");
+        signal2.classList.remove("sig2");
+        signal2.classList.add("sig1");
+        signal3.classList.remove("sig1");
+        signal3.classList.add("sig2");
+        signal4.classList.remove("sig1");
+        signal4.classList.add("sig2");
+    }
+    else if (signal2.classList.contains("sig1")) {
+        signal1.classList.remove("sig1");
+        signal1.classList.add("sig2");
+        signal2.classList.remove("sig1");
+        signal2.classList.add("sig2");
+        signal3.classList.remove("sig2");
+        signal3.classList.add("sig1");
+        signal4.classList.remove("sig1");
+        signal4.classList.add("sig2"); 
+    }
+    else if (signal3.classList.contains("sig1")) {
+        signal1.classList.remove("sig1");
+        signal1.classList.add("sig2");
+        signal2.classList.remove("sig1");
+        signal2.classList.add("sig2");
+        signal3.classList.remove("sig1");
+        signal3.classList.add("sig2"); 
+        signal4.classList.remove("sig2");
+        signal4.classList.add("sig1"); 
+    }
+    else if (signal4.classList.contains("sig1")) {
+        signal1.classList.remove("sig2");
+        signal1.classList.add("sig1");
+        signal2.classList.remove("sig1");
+        signal2.classList.add("sig2");
+        signal3.classList.remove("sig1");
+        signal3.classList.add("sig2"); 
+        signal4.classList.remove("sig1");
+        signal4.classList.add("sig2");
+         
+    }
+    
+})
+atras.addEventListener("click", function(){
+    if (signal1.classList.contains("sig1")) {
+        signal1.classList.remove("sig1");
+        signal1.classList.add("sig2");
+        signal2.classList.remove("sig1");
+        signal2.classList.add("sig2");
+        signal3.classList.remove("sig1");
+        signal3.classList.add("sig2");
+        signal4.classList.remove("sig2");
+        signal4.classList.add("sig1");
+    }
+    else if (signal2.classList.contains("sig1")) {
+        signal1.classList.remove("sig2");
+        signal1.classList.add("sig1");
+        signal2.classList.remove("sig1");
+        signal2.classList.add("sig2");
+        signal3.classList.remove("sig1");
+        signal3.classList.add("sig2");
+        signal4.classList.remove("sig1");
+        signal4.classList.add("sig2"); 
+    }
+    else if (signal3.classList.contains("sig1")) {
+        signal1.classList.remove("sig1");
+        signal1.classList.add("sig2");
+        signal2.classList.remove("sig2");
+        signal2.classList.add("sig1");
+        signal3.classList.remove("sig1");
+        signal3.classList.add("sig2"); 
+        signal4.classList.remove("sig1");
+        signal4.classList.add("sig2"); 
+    }
+    else if (signal4.classList.contains("sig1")) {
+        signal1.classList.remove("sig1");
+        signal1.classList.add("sig2");
+        signal2.classList.remove("sig1");
+        signal2.classList.add("sig2");
+        signal3.classList.remove("sig2");
+        signal3.classList.add("sig1"); 
+        signal4.classList.remove("sig1");
+        signal4.classList.add("sig2");
+         
+    }
+    
+})
+
+
+
+
+// siguiente.addEventListener("click", function(){
+//     if (signal1.classList.contains("sig2")) {
+//         signal1.classList.remove("sig2");
+//         signal1.classList.add("sig1");
+//     }
+//     else if (signal2.classList.contains("sig2")) {
+//         signal2.classList.remove("sig2");
+//         signal2.classList.add("sig1"); 
+//     }
+//     else if (signal3.classList.contains("sig2")) {
+//         signal3.classList.remove("sig2");
+//         signal3.classList.add("sig1"); 
+//     }
+//     else if (signal4.classList.contains("sig2")) {
+//         signal4.classList.remove("sig2");
+//         signal4.classList.add("sig1"); 
+//     }
+    
+// })
+// atras.addEventListener("click", function(){
+//     if (signal1.classList.contains("sig1")) {
+//         signal1.classList.remove("sig1");
+//         signal1.classList.add("sig2");
+//     }
+//     else if (signal2.classList.contains("sig1")) {
+//         signal2.classList.remove("sig1");
+//         signal2.classList.add("sig2"); 
+//     }
+//     else if (signal3.classList.contains("sig1")) {
+//         signal3.classList.remove("sig1");
+//         signal3.classList.add("sig2"); 
+//     }
+//     else if (signal4.classList.contains("sig1")) {
+//         signal4.classList.remove("sig1");
+//         signal4.classList.add("sig2"); 
+//     }
+
+// })
 
 
