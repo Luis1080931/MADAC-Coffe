019fa4ea369222d33c56c759132f1a274313bf7c
LobitoM365 <almariojuancho@gmail.com>
2023-09-27 12:46:26 -0500

    sladersergio

--- js/iconos.js
+++
@@ -1,9 +0,0 @@
-var boton = document.getElementById('btn-menu')
-var barra = document.querySelector('cuadro')
-
-function toggleBarra() {
-    barra.classList.toggle("close")
-}
-barra.classList.add("close")
-
-boton.addEventListener('click', toggleBarra)
